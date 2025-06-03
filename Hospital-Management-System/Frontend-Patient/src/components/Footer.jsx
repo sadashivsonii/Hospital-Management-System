a89868276933fea7834b8d80fb405999e86852b7
sadashivsonii <sadashivsoni2004@gmail.com>
2025-06-04 01:10:02 +0530

    Add files via upload

---
+++ Hospital-Management-System/Frontend-Patient/src/components/Footer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaLocationDot, FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaGithub, FaInstagramSquare } from "react-icons/fa";
+import { RxLinkedinLogo } from "react-icons/rx";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="container">
+        <hr />
+        <div className="content">
+          <div>
+            <img src="/logo.png" alt="logo" className="logo-img" />
+          </div>
+          <div>
+            <h4>Quick Links</h4>
+            <ul>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/appointment"}>Appointment</Link>
+              <Link to={"/about"}>About Us</Link>
+            </ul>
+          </div>
+          <div>
+            <h4>Developed by</h4>
+            <p>Sadashiv Soni</p>
+            <div className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/sadashivsoni"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <RxLinkedinLogo />
+              </a>
+              <a
+                href="https://github.com/sadashivsonii"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://www.instagram.com/i_shivamsoni?igsh=aHRvYXZkcWxnMTJ4&utm_source=qr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
+          <div>
+            <h4>Contact</h4>
+            <div>
+              <FaPhone />
+              <span>999-999-9999</span>
+            </div>
+            <div>
+              <MdEmail />
+              <span>lifecare@gmail.com</span>
+            </div>
+            <div>
+              <FaLocationDot />
+              <span>Greater Noida, India</span>
+            </div>
+          </div>
+        </div>
+        <div className="copyright">
+          <p>&copy; {new Date().getFullYear()} Life Care Hospital</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
